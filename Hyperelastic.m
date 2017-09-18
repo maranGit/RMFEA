4d05dc0be1f4d4536a5416bbab57b378c29eebba
Ran <maran1990@qq.com>
2017-09-17 23:05:47 -0400

    Hyperelastic material model from Homework 2 in NLFEA class

---
+++ Hyperelastic.m
@@ -0,0 +1,45 @@
+% small strain hyperelastic material
+% HW2 in NLFEA class
+% W(eps) = a*eps_mm*log(1+eps_nn)/2 + 3*b/*eps_mn*eps_mn/2
+% Input: strain (Voigt notation)
+% Output: (1) stress; (2) tangent stiffness. (Voigt notation)
+classdef Hyperelastic < MaterialModel
+    properties
+        a
+        b
+    end
+    methods
+        function obj = Hyperelastic(inp)
+            if nargin == 0
+                obj.a = 40;
+                obj.b = 60;
+            elseif nargin == 1
+                if ~isequal(size(inp),[2,1])
+                    error('Material parameters of Hyperelastic material shall be a 2x1 vector');
+                end
+                obj.a=inp(1);
+                obj.b=inp(2);
+            else
+                error('Invalid input for Linear Hyperelastic material');
+            end
+        end
+        function [D, sigma] = calcStressTangent(obj, eps)
+            if isequal(size(eps),[3,1])
+                traceEps = eps(1) + eps(2);
+                temp = 0.5*obj.a*(2+traceEps)/(1+traceEps)^2;
+                D = [temp+6*obj.b, temp, 0;
+                     temp, temp+6*obj.b, 0;
+                     0, 0, 3*obj.b];
+                temp = 0.5 * obj.a * (log(1+traceEps) + traceEps/(1+traceEps));
+                sigma = [temp + 6*obj.b*eps(1);
+                         temp + 6*obj.b*eps(2);
+                         3 * obj.b * eps(3)];
+            elseif isequal(size(eps), [6,1])
+                D = 0;
+                sigma = 0;
+            else
+                error('strain must be Voigt notation')
+            end
+        end
+    end
+end
